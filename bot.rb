d2831b22b48d12a9b653c592329c1cdb0c3d183b
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-08-02 23:10:58 +0100

    Updated code block with geocode integration

--- bot.rb
+++ bot.rb
@@ -3,24 +3,31 @@ require "json"
 require "httparty"
 include Facebook::Messenger
 
-API_URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+map_url = "https://maps.googleapis.com/maps/api/geocode/json"
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
   puts "Received '#{message.inspect}' from #{message.sender}"
-  parsed_response = get_parsed_response(API_URL, message.text)
+  parsed_response = get_parsed_response(map_url, message.text)
   message.type
   coords = extract_coordinates(parsed_response)
-  message.reply(text: "Latitude: #{coord["lat"]}, Longitude: #{coord["lng"]}")
+  message.reply(text: coords)
 end
 
 def get_parsed_response(url, query)
-  response = HTTParty.get(url + query)
+  response = HTTParty.get(url,
+    query: {
+      address: location,
+      key: ENV["GOOGLE_API_KEY"]
+    })
   parsed = JSON.parse(response.body)
   parsed["status"] != "ZERO_RESULTS" ? parsed : nil
 end
 
 def extract_coordinates(parsed)
-  parsed["results"].first["geometry"]["location"]
+  # parsed["results"].first["geometry"]["location"]
+
+  parsed["results"][0]["geometry"]["bounds"]["northeast"]["lat"].round(1).to_s + ", " +
+    parsed["results"][0]["geometry"]["bounds"]["northeast"]["lng"].round(1).to_s
 end
