ce77b0cd3bea8e507aa4ad5596eb2731b60934c8
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-08-02 23:07:39 +0100

    Added test for geocode

--- lib/geocode.rb
+++ lib/geocode.rb
@@ -1,9 +1,18 @@
+require "httparty"
+require "pp"
+
 class Geocode
   def get_geocode(location)
-    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}"
-    data = open(url).read
+    url = "https://maps.googleapis.com/maps/api/geocode/json"
+    # ?address=#{location}&key=#{ENV["GOOGLE_API_KEY"]}"
+    data = HTTParty.get(url, format: :plain,
+      query: {
+        address: location,
+        key: ENV["GOOGLE_API_KEY"]
+        })
     decoded_data = JSON.parse(data)
-    return data["results"][0]["geometry"]["bounds"]["northeast"]["lat"] + ", " +
-    data["results"][0]["geometry"]["bounds"]["northeast"]["lng"]
+    pp decoded_data
+    return decoded_data["results"][0]["geometry"]["bounds"]["northeast"]["lat"].round(1).to_s + ", " +
+    decoded_data["results"][0]["geometry"]["bounds"]["northeast"]["lng"].round(1).to_s
   end
 end
--- spec/geocode_spec.rb
+++ spec/geocode_spec.rb
@@ -3,7 +3,7 @@ require_relative "../lib/geocode"
 describe Geocode do
   it "it should return coordinates" do
     geocode = Geocode.new
-    result = geocode.get_geocode
-    expect(result("london")).to eq("51.5" ", " "0.1")
+    result = geocode.get_geocode("london")
+    expect(result).to eq("51.7" + ", " + "0.1")
   end
 end
